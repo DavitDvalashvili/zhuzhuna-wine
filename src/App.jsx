18fe4498f0999a1d47ae076ce0c0322ddf4822b7
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-07-18 19:51:49 +0400

    chore: add initial files install react and tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Layout/Footer";
+import Header from "./Layout/Header";
+import Main from "./Layout/Main";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
