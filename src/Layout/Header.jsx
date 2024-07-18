ce61e23c4d5f4bcf85a7fca91076e23e43ae5543
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-07-18 21:12:34 +0400

    feat: add useCountry custom hook for retrieving the user location

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,5 +1,10 @@
+import useCountry from "../hooks/useCountry.jsx";
+
 const Header = () => {
-  return <div>header</div>;
+  const { country } = useCountry();
+  console.log(country);
+
+  return <div>{country}</div>;
 };
 
 export default Header;
---
+++ src/hooks/useCountry.jsx
@@ -0,0 +1,36 @@
+// useCountry.js
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useCountry = () => {
+  const [country, setCountry] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get("https://ipinfo.io/json", {
+          params: {
+            token: "376da34da6a2c2",
+          },
+        });
+
+        console.log(response.data); // Log the entire response to debug
+
+        if (response.data && response.data.country) {
+          setCountry(response.data.country);
+        } else {
+          setError("Country name not found in the response.");
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchCountry();
+  }, []);
+
+  return { country, error };
+};
+
+export default useCountry;
